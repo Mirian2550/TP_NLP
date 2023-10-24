89e7a7dd76b776e50d0e0b442411a98411f5fb33
mirian2550 <miri2550@gmail.com>
2023-10-24 00:27:19 -0300

    scrapper food

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ output_file = 'data/dataset.csv'
 security_url = 'url1'
 technology_url = 'url2'
 sports_url = 'url3'
-food_url = 'url4'
+food_url = 'https://www.recetasnestle.com.mx/sitemap.xml'
 
 scraper = Scraper(output_file, security_url, technology_url, sports_url, food_url)
 scraper.run_scrapers()
--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -2,7 +2,9 @@ import csv
 import threading
 import random
 import requests
-
+from bs4 import BeautifulSoup
+import xml.etree.ElementTree as ET
+import logging
 
 class Scraper:
     def __init__(self, output_file, security_url, technology_url, sports_url, food_url, counts=10):
@@ -45,49 +47,101 @@ class Scraper:
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36',
         ]
 
+        logging.basicConfig(filename='scraper.log', level=logging.INFO, format='%(asctime)s [%(levelname)s]: %(message)s')
+
     def get_random_user_agent(self):
         return random.choice(self.user_agents)
 
     def write_to_csv(self, data):
-        with open(self.output_file, 'w', newline='') as csvfile:
-            fieldnames = ['title', 'url', 'text']  # Estructura del CSV: título, URL, texto
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        try:
+            with open(self.output_file, 'a', newline='', encoding='utf-8') as csvfile:
+                fieldnames = ['title', 'url', 'text']  # Estructura del CSV: título, URL, texto
+                # Configurar el delimitador y el encoding
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
 
-            writer.writeheader()
+                # Si el archivo está vacío, escribe la cabecera
+                if csvfile.tell() == 0:
+                    writer.writeheader()
 
-            for item in data:
+                # Agrega la nueva fila al archivo
                 writer.writerow({
-                    'title': item['title'],
-                    'url': item['url'],
-                    'text': item['text']
+                    'title': data['title'],
+                    'url': data['url'],
+                    'text': data['text']
                 })
+        except Exception as e:
+            logging.error(f"Error al escribir en el archivo CSV: {e}")
 
     def scrape_security_news(self):
-        headers = {'User-Agent': self.get_random_user_agent()}
-        response = requests.get(self.security_url, headers=headers)
-        pass
+        try:
+            headers = {'User-Agent': self.get_random_user_agent()}
+            response = requests.get(self.security_url, headers=headers)
+            pass
+        except Exception as e:
+            logging.error(f"Error al obtener noticias de seguridad: {e}")
 
     def scrape_technology_news(self):
-        headers = {'User-Agent': self.get_random_user_agent()}
-        response = requests.get(self.technology_url, headers=headers)
-        pass
+        try:
+            headers = {'User-Agent': self.get_random_user_agent()}
+            response = requests.get(self.technology_url, headers=headers)
+            pass
+        except Exception as e:
+            logging.error(f"Error al obtener noticias de tecnología: {e}")
 
     def scrape_sports_news(self):
-        headers = {'User-Agent': self.get_random_user_agent()}
-        response = requests.get(self.sports_url, headers=headers)
-        pass
+        try:
+            headers = {'User-Agent': self.get_random_user_agent()}
+            response = requests.get(self.sports_url, headers=headers)
+            pass
+        except Exception as e:
+            logging.error(f"Error al obtener noticias de deportes: {e}")
 
     def scrape_food_news(self):
-        headers = {'User-Agent': self.get_random_user_agent()}
-        response = requests.get(self.food_url, headers=headers)
-        pass
+        try:
+            headers = {'User-Agent': self.get_random_user_agent()}
+            response = requests.get(self.food_url, headers=headers)
+            if response.status_code == 200:
+                root = ET.fromstring(response.content)
+
+                url_list = []
+                for url_elem in root.findall(".//{http://www.sitemaps.org/schemas/sitemap/0.9}url"):
+                    loc_elem = url_elem.find("{http://www.sitemaps.org/schemas/sitemap/0.9}loc")
+                    if loc_elem is not None:
+                        url_list.append(loc_elem.text)
+
+                for url in url_list[1:self.counts]:
+                    headers = {'User-Agent': self.get_random_user_agent()}
+                    response_url = requests.get(url, headers=headers)
+                    if response_url.status_code == 200:
+                        soup = BeautifulSoup(response_url.text, 'html.parser')
+
+                        div_with_classes = soup.find('div', class_='container container--no-padding-md')
+                        if div_with_classes:
+                            paragraphs = div_with_classes.find_all('p')
+                            titulo = soup.h1.text
+                            titulo = titulo.replace('\n', '')
+                            texto = ''
+                            for paragraph in paragraphs:
+                                texto += paragraph.text
+                            texto = texto.replace('\n', '')
+                            self.write_to_csv({
+                                'title': titulo,
+                                'url': url,
+                                'text': texto
+                            })
+                        else:
+                            logging.warning(f"Div with classes not found in {url}")
+                    else:
+                        logging.error(f"Failed to retrieve {url}. Status code: {response_url.status_code}")
+        except Exception as e:
+            logging.error(f"Error en la función de scraping de noticias de comida: {e}")
 
     def run_scrapers(self):
         # Crear hilos para ejecutar cada scraper
         threads = []
-        threads.append(threading.Thread(target=self.scrape_security_news))
-        threads.append(threading.Thread(target=self.scrape_technology_news))
-        threads.append(threading.Thread(target=self.scrape_sports_news))
+        #threads.append(threading.Thread(target=self.scrape_security_news))
+        #threads.append(threading.Thread(target=self.scrape_technology_news))
+        #threads.append(threading.Thread(target=self.scrape_sports_news))
         threads.append(threading.Thread(target=self.scrape_food_news))
 
         # Iniciar los hilos
