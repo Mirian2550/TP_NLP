303f7cb23deec6645df7e46b1f98f152ca1d8d6a
pepe <dani_rc_22@hotmail.com>
2023-10-24 16:18:34 -0300

    se agrego modulo clasificador

--- clasificador/clasificador.py
+++ clasificador/clasificador.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 class SVMClassifier:
     def __init__(self, file_path, kernel='linear', C=1.0):
@@ -10,17 +11,19 @@ class SVMClassifier:
         self.model = svm.SVC(kernel=kernel, C=C)
         self.dataset = self.load_dataset()
         self.label_encoder = LabelEncoder()
+        self.vectorizer = TfidfVectorizer()
 
     def load_dataset(self):
-        return pd.read_csv(self.file_path, delimiter= '|')
+        return pd.read_csv(self.file_path, delimiter='|')
 
     def preprocess_data(self):
         X = self.dataset['text']
         y_str = self.dataset['category']
 
         y = self.label_encoder.fit_transform(y_str)
+        X_vectorized = self.vectorizer.fit_transform(X)
 
-        return X, y
+        return X_vectorized, y
 
     def train(self):
         X, y = self.preprocess_data()
@@ -35,5 +38,3 @@ class SVMClassifier:
         y_pred = self.predict()
         accuracy = accuracy_score(y_true, y_pred)
         return accuracy
-
-
